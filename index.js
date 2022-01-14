c3ee19acf61a75c8d472b6be434c082547d5493e
Hamzah Contreras <Hamzah_36@outlook.com>
2022-01-13 19:53:38 -0600

    commit tree bba08a14247754223d3cb4a3bfec102a80c28ba1

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    const nameList = []
+
+    for(let i = 0; i < names.length; i++) {
+        nameList.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return nameList
+}
+
+function countDown() {
+    let counter = 10
+    while(counter >= 0) {
+        console.log(counter--)
+    }
+    return counter
+}
